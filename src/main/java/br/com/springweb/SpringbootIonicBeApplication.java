103a36464211b7ee3044ca329f161ac2136060e6
Giovani Silva <giovani-silva1@live.com>
2021-05-05 01:53:58 -0300

    Criacao do projeto backend

---
+++ src/main/java/br/com/springweb/SpringbootIonicBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIonicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIonicBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springweb/SpringbootIonicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIonicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
