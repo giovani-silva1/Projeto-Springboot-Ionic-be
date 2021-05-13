44c8ddcdc40acb35b13adeaf6356a22925310d06
Giovani Silva <giovani-silva1@live.com>
2021-05-13 15:53:50 -0300

    inserido o cascade.All em endereco de cliente para quando deletar um cliente seja deletado tambem seu endereco

--- src/main/java/br/com/springweb/entities/Cliente.java
+++ src/main/java/br/com/springweb/entities/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -34,8 +35,9 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
 
+	
 	@JsonManagedReference
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
