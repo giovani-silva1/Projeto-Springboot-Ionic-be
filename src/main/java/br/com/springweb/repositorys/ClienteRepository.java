e7498a2244b115cf4b71547162d4dcb22059db79
Giovani Silva <giovani-silva1@live.com>
2021-05-13 18:29:03 -0300

    Criado a validacao personalizada de validar o cadastro unico de email

--- src/main/java/br/com/springweb/repositorys/ClienteRepository.java
+++ src/main/java/br/com/springweb/repositorys/ClienteRepository.java
@@ -1,11 +1,15 @@
 package br.com.springweb.repositorys;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.springweb.entities.Cliente;
-
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/br/com/springweb/service/validators/AdicionarClienteValidator.java
+++ src/main/java/br/com/springweb/service/validators/AdicionarClienteValidator.java
@@ -2,15 +2,25 @@ package br.com.springweb.service.validators;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.springweb.controllers.exception.FieldError;
+import br.com.springweb.entities.Cliente;
 import br.com.springweb.entities.dto.ClienteNovoDTO;
 import br.com.springweb.entities.enums.TipoCliente;
+import br.com.springweb.repositorys.ClienteRepository;
 import br.com.springweb.utils.ValidadorCPF_CNPJ;
 
 public class AdicionarClienteValidator implements ConstraintValidator<AdicionarCliente, ClienteNovoDTO> {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	
 	@Override
 	public void initialize(AdicionarCliente ann) {
 	}
@@ -28,6 +38,12 @@ public class AdicionarClienteValidator implements ConstraintValidator<AdicionarC
 				&& !ValidadorCPF_CNPJ.isValidCnpj(clienteNovoDTO.getCpfOuCnpj())) {
 			list.add(new FieldError("CpfOuCnpj", "CNPJ invalido"));
 		}
+		
+		Cliente clienteEmail = clienteRepository.findByEmail(clienteNovoDTO.getEmail());
+		if(clienteEmail != null) {
+			list.add(new FieldError("Email","Email ja foi cadastrado na base de dados"));
+		}
+		
 
 		for (FieldError e : list) {
 			context.disableDefaultConstraintViolation();
